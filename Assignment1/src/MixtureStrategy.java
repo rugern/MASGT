849347506d390d0f3baa79b36e62d0460e0fdd76
Nils Inge Rugsveen <nirugsveen@gmail.com>
2015-01-22 17:12:20 +0100

    Finished the remaining strats

--- Assignment1/src/MixtureStrategy.java
+++ Assignment1/src/MixtureStrategy.java
@@ -3,8 +3,17 @@ import java.util.List;
 
 public class MixtureStrategy extends Player {
 	
+	public MixtureStrategy(String name) {
+		super(name);
+	}
+	
 	@Override
 	public Action dilemma(List<Action> opponentPreviousActions) {
-		
+		if(Math.random()<0.5) {
+			return Action.COOPERATE;
+		}
+		else {
+			return Action.DEFECT;
+		}
 	}
 }
